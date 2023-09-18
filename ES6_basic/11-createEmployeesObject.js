8e1d867d677cbe63e74e42be8c768d86feceee45
RaptorZ98 <federicoheuercor@hotmail.com>
2023-09-17 19:52:26 -0700

    m

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const res = {
-    [departmentName]: employees
+    [departmentName]: employees,
   };
-  return res
+  return res;
 }
