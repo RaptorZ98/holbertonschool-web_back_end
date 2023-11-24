3aa20d6f682ac33a59136302087f7861fb7e73de
RaptorZ98 <federicoheuercor@hotmail.com>
2023-11-23 21:17:25 -0800

    added 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter(({ location }) => location === city).map((student) => {
+    const find = newGrades.find((grade) => grade.studentId === student.id);
+    student.grade = 'N/A';
+    if (find) {
+      student.grade = find.grade;
+    }
+    return student;
+  });
+}
